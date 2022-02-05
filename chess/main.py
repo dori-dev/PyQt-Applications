d8387f07f66bf121a761fd2494d9c34a62fc0c99
Mohammad Dori <mr.dori.dev@gmail.com>
2022-02-05 16:12:25 +0330

    Add files via upload

---
+++ chess/main.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        view = QtWebEngineWidgets.QWebEngineView()
+        file = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "index.html"
+        )
+        view.load(QtCore.QUrl.fromLocalFile(file))
+        self.setWindowTitle("Chess")
+        self.setCentralWidget(view)
+        self.resize(640, 480)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
